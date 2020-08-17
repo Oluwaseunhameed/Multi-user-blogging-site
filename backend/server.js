4861260621e7f4a24ae8b88ca765512cfabd6fe9
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-08-17 13:57:04 +0100

    connected to DB and structured files

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 require('dotenv').config()
 
+// bring routes
+const blogRoutes = require('./routes/blog')
 
 // app
 const app = express()
 
+// db
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
+    .then(() => console.log('Database connected!'))
 // middlewares
 app.use(morgan('dev'))
 app.use(bodyParser.json())
@@ -17,6 +23,8 @@ app.use(cookieParser())
 if (process.env.NODE_ENV === 'development') {
     app.use(cors({ origin: `${process.env.CLIENT_URL}` }))
 }
+
+app.use('/api', blogRoutes)
 
 
 // routes
